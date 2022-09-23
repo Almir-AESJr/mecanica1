7cd773443b02fcd70dce23d01539ef62791335e7
Almir-AESJr <almir.aesjunior@gmail.com>
2022-09-23 18:06:18 -0300

    Almir commit

---
+++ conexao.php
@@ -0,0 +1,14 @@
+<?php
+const SERVIDOR = "localhost:3306";
+
+const BANCO = "mecanica_v2";
+
+const USUARIO = "root";
+
+const SENHA = "";
+
+const REGISTROS_POR_PAGINA = 5;
+
+//$CONEXAO = mysqli_connect($SERVIDOR, $BANCO, $USUARIO, $SENHA);
+
+?>
